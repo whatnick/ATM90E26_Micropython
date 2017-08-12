81cdde95577d47900d9e47ee404a44ad75b5f402
Tisham Dhar <whatnickd@gmail.com>
2017-08-12 23:13:40 +0930

    Made energy ic an object

--- atm90e26_SPI.py
+++ atm90e26_SPI.py
@@ -56,6 +56,139 @@ PowerFTwo = 0x6D # N Line Power Factor
 PangleTwo = 0x6E # Phase Angle between Voltage and N Line Current
 SmeanTwo = 0x6F # N Line Mean Apparent Power
 
+
+class ATM90E26_SPI:
+	
+	'''       
+	spi - hardware or software SPI implementation
+	cs - Chip Select pin
+	'''
+	def __init__(self,spi,cs):
+		self.spi = spi
+		self.cs = cs
+		self.init_atm90()
+	'''
+	rw - True - read, False - write
+	address - register to operate
+	val - value to write (if any)
+	'''
+	def comm_atm90(self,RW,address,val):
+		#switch MSB and LSB of value
+		buf = bytearray(2)
+		otw_val = struct.pack('>H',val)
+		#Set read write flag
+		address|=RW<<7
+		self.cs.value(False)
+		time.sleep_us(10)
+		self.spi.write(bytearray([address]))
+		''' Must wait 4 us for data to become valid '''
+		time.sleep_us(4)
+
+		#Read data
+		#Do for each byte in transfer
+		if(RW):
+			buf = self.spi.read(2)
+		else:
+			self.spi.write(otw_val)			 # write all the bytes
+		self.cs.value(1)
+		return int.from_bytes(buf,'big')
+
+	def init_atm90(self):
+		self.comm_atm90(False,SoftReset,0x789A)	#Perform soft reset
+		self.comm_atm90(False,FuncEn,0x0030)	#Voltage sag irq=1, report on warnout pin=1, energy dir change irq=0
+		self.comm_atm90(False,SagTh,0x1F2F)	#Voltage sag threshhold
+		
+		#Set metering calibration values
+		self.comm_atm90(False,CalStart,0x5678) #Metering calibration startup command. Register 21 to 2B need to be set
+		self.comm_atm90(False,PLconstH,0x00B9) #PL Constant MSB
+		self.comm_atm90(False,PLconstL,0xC1F3) #PL Constant LSB
+		self.comm_atm90(False,Lgain,0x1D39)   #Line calibration gain
+		self.comm_atm90(False,Lphi,0x0000) #Line calibration angle
+		self.comm_atm90(False,PStartTh,0x08BD) #Active Startup Power Threshold
+		self.comm_atm90(False,PNolTh,0x0000) #Active No-Load Power Threshold
+		self.comm_atm90(False,QStartTh,0x0AEC) #Reactive Startup Power Threshold
+		self.comm_atm90(False,QNolTh,0x0000) #Reactive No-Load Power Threshold
+		self.comm_atm90(False,MMode,0x9422) #Metering Mode Configuration. All defaults. See pg 31 of datasheet.
+		self.comm_atm90(False,CSOne,0x4A34) #Write CSOne, as self calculated
+		
+		print("Checksum 1:")
+		print(hex(self.comm_atm90(True,CSOne,0x0000))) #Checksum 1. Needs to be calculated based off the above values.
+		
+		
+		#Set measurement calibration values
+		self.comm_atm90(False,AdjStart,0x5678) #Measurement calibration startup command, registers 31-3A
+		self.comm_atm90(False,Ugain,0xD464)    #Voltage rms gain
+		self.comm_atm90(False,IgainL,0x6E49)   #L line current gain
+		self.comm_atm90(False,Uoffset,0x0000)  #Voltage offset
+		self.comm_atm90(False,IoffsetL,0x0000) #L line current offset
+		self.comm_atm90(False,PoffsetL,0x0000) #L line active power offset
+		self.comm_atm90(False,QoffsetL,0x0000) #L line reactive power offset
+		self.comm_atm90(False,CSTwo,0xD294) #Write CSTwo, as self calculated
+		
+		print("Checksum 2:")
+		print(hex(self.comm_atm90(True,CSTwo,0x0000)))    #Checksum 2. Needs to be calculated based off the above values.
+		
+		self.comm_atm90(False,CalStart,0x8765) #Checks correctness of 21-2B registers and starts normal metering if ok
+		self.comm_atm90(False,AdjStart,0x8765) #Checks correctness of 31-3A registers and starts normal measurement  if ok
+		
+		systemstatus = self.GetSysStatus()
+		
+		if (systemstatus&0xC000):
+			#checksum 1 error
+			print("Checksum 1 Error!!")
+		if (systemstatus&0x3000):
+			#checksum 2 error
+			print("Checksum 2 Error!!")
+
+	def GetSysStatus(self):
+		return self.comm_atm90(True,SysStatus,0x0000)
+			
+
+	def GetLineVoltage(self):
+		voltage = self.comm_atm90(True,Urms,0xFFFF)
+		return voltage/100.0
+
+
+	def GetMeterStatus(self):
+	  return self.comm_atm90(True,EnStatus,0xFFFF)
+
+
+	def GetLineCurrent(self):
+		current = self.comm_atm90(True,Irms,0xFFFF)
+		return current/1000.0
+
+
+	def GetActivePower(self):
+		apower = self.comm_atm90(True,Pmean,0xFFFF) #Complement, MSB is signed bit
+		return double(apower)
+
+
+	def GetFrequency(self):
+		freq = self.comm_atm90(True,Freq,0xFFFF)
+		return freq/100.0
+
+
+	def GetPowerFactor(self):
+		pf = self.comm_atm90(True,PowerF,0xFFFF) #MSB is signed bit
+		#if negative
+		if(pf&0x8000):
+			pf=(pf&0x7FFF)*-1
+		
+		return pf/1000.0
+
+
+	def GetImportEnergy(self):
+		#Register is cleared after reading
+		ienergy = self.comm_atm90(True,APenergy,0xFFFF)
+		return ienergy*0.0001 #returns kWh if PL constant set to 1000imp/kWh
+
+
+	def GetExportEnergy(self):
+		#Register is cleared after reading
+		eenergy = self.comm_atm90(True,ANenergy,0xFFFF)
+		return eenergy*0.0001 #returns kWh if PL constant set to 1000imp/kWh
+	
+
 sck = machine.Pin(5,machine.Pin.OUT)
 mosi = machine.Pin(18,machine.Pin.OUT)
 miso = machine.Pin(19,machine.Pin.IN)
@@ -64,134 +197,11 @@ cs2 = machine.Pin(33,machine.Pin.OUT)
 
 spi = machine.SPI(1,baudrate=200000,bits=8,polarity=1,phase=1,firstbit=machine.SPI.MSB,sck=sck,mosi=mosi,miso=miso)
 
-'''
-rw - True - read, False - write
-address - register to operate
-val - value to write (if any)
-cs - chip select when multiplexing
-'''
-def comm_atm90(RW,address,val,cs):
-	#switch MSB and LSB of value
-	buf = bytearray(2)
-	otw_val = struct.pack('>H',val)
-	#Set read write flag
-	address|=RW<<7
-	cs.value(False)
-	time.sleep_us(10)
-	spi.write(bytearray([address]))
-	''' Must wait 4 us for data to become valid '''
-	time.sleep_us(4)
-
-	#Read data
-	#Do for each byte in transfer
-	if(RW):
-		buf = spi.read(2)
-	else:
-		spi.write(otw_val)			 # write all the bytes
-	cs.value(1)
-	return int.from_bytes(buf,'big')
-	
-def GetSysStatus(cs):
-	return comm_atm90(True,SysStatus,0x0000,cs)
-
-def init_atm90(cs):
-	comm_atm90(False,SoftReset,0x789A,cs)	#Perform soft reset
-	comm_atm90(False,FuncEn,0x0030,cs)	#Voltage sag irq=1, report on warnout pin=1, energy dir change irq=0
-	comm_atm90(False,SagTh,0x1F2F,cs)	#Voltage sag threshhold
-	
-	#Set metering calibration values
-	comm_atm90(False,CalStart,0x5678,cs) #Metering calibration startup command. Register 21 to 2B need to be set
-	comm_atm90(False,PLconstH,0x00B9,cs) #PL Constant MSB
-	comm_atm90(False,PLconstL,0xC1F3,cs) #PL Constant LSB
-	comm_atm90(False,Lgain,0x1D39,cs)   #Line calibration gain
-	comm_atm90(False,Lphi,0x0000,cs) #Line calibration angle
-	comm_atm90(False,PStartTh,0x08BD,cs) #Active Startup Power Threshold
-	comm_atm90(False,PNolTh,0x0000,cs) #Active No-Load Power Threshold
-	comm_atm90(False,QStartTh,0x0AEC,cs) #Reactive Startup Power Threshold
-	comm_atm90(False,QNolTh,0x0000,cs) #Reactive No-Load Power Threshold
-	comm_atm90(False,MMode,0x9422,cs) #Metering Mode Configuration. All defaults. See pg 31 of datasheet.
-	comm_atm90(False,CSOne,0x4A34,cs) #Write CSOne, as self calculated
-	
-	print("Checksum 1:")
-	print(hex(comm_atm90(True,CSOne,0x0000,cs))) #Checksum 1. Needs to be calculated based off the above values.
-	
-	
-	#Set measurement calibration values
-	comm_atm90(False,AdjStart,0x5678,cs) #Measurement calibration startup command, registers 31-3A
-	comm_atm90(False,Ugain,0xD464,cs)    #Voltage rms gain
-	comm_atm90(False,IgainL,0x6E49,cs)   #L line current gain
-	comm_atm90(False,Uoffset,0x0000,cs)  #Voltage offset
-	comm_atm90(False,IoffsetL,0x0000,cs) #L line current offset
-	comm_atm90(False,PoffsetL,0x0000,cs) #L line active power offset
-	comm_atm90(False,QoffsetL,0x0000,cs) #L line reactive power offset
-	comm_atm90(False,CSTwo,0xD294,cs) #Write CSTwo, as self calculated
-	
-	print("Checksum 2:")
-	print(hex(comm_atm90(True,CSTwo,0x0000,cs)))    #Checksum 2. Needs to be calculated based off the above values.
-	
-	comm_atm90(False,CalStart,0x8765,cs) #Checks correctness of 21-2B registers and starts normal metering if ok
-	comm_atm90(False,AdjStart,0x8765,cs) #Checks correctness of 31-3A registers and starts normal measurement  if ok
-	
-	systemstatus = GetSysStatus(cs)
-	
-	if (systemstatus&0xC000):
-		#checksum 1 error
-		print("Checksum 1 Error!!")
-	if (systemstatus&0x3000):
-		#checksum 2 error
-		print("Checksum 2 Error!!")
-		
-
-def GetLineVoltage(cs):
-	voltage=comm_atm90(True,Urms,0xFFFF,cs)
-	return voltage/100.0
-
-
-def GetMeterStatus():
-  return comm_atm90(True,EnStatus,0xFFFF)
-
-
-def GetLineCurrent(cs):
-	current=comm_atm90(True,Irms,0xFFFF,cs)
-	return current/1000.0
-
-
-def GetActivePower(cs):
-	apower = comm_atm90(True,Pmean,0xFFFF,cs) #Complement, MSB is signed bit
-	return double(apower)
-
-
-def GetFrequency(cs):
-	freq = comm_atm90(True,Freq,0xFFFF,cs)
-	return freq/100.0
-
-
-def GetPowerFactor(cs):
-	pf = comm_atm90(True,PowerF,0xFFFF,cs) #MSB is signed bit
-	#if negative
-	if(pf&0x8000):
-		pf=(pf&0x7FFF)*-1
-	
-	return pf/1000.0
-
-
-def GetImportEnergy(cs):
-	#Register is cleared after reading
-	ienergy = comm_atm90(True,APenergy,0xFFFF,cs)
-	return ienergy*0.0001 #returns kWh if PL constant set to 1000imp/kWh
-
-
-def GetExportEnergy(cs):
-	#Register is cleared after reading
-	eenergy = comm_atm90(True,ANenergy,0xFFFF,cs)
-	return eenergy*0.0001 #returns kWh if PL constant set to 1000imp/kWh
-	
-
-init_atm90(cs2)
+energy_ic = ATM90E26_SPI(spi,cs2)
 
 while True:
-	val = GetSysStatus(cs2)
+	val = energy_ic.GetSysStatus()
 	print(hex(val))
-	voltage = GetLineVoltage(cs2)
+	voltage = energy_ic.GetLineVoltage()
 	print(voltage)
 	time.sleep_ms(100)
